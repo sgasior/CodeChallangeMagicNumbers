56ca3c297e98ab8d546e2eccaf868cdb7bc5266a
Szymon Gąsior <szgasior@outlook.com>
2019-01-29 20:23:27 +0100

    Started to using FileTypeCheck interface instead of FileTypeCheckImpl in Main

--- src/gasior/szymon/epam/Main.java
+++ src/gasior/szymon/epam/Main.java
@@ -14,24 +14,24 @@ public class Main {
         // test6.txt has got bad extension
 
         try {
-            FileTypeCheckImpl fileTypeCheck = new FileTypeCheckImpl(new File("resources/test1.jpg"));
+            FileTypeCheck fileTypeCheck = new FileTypeCheckImpl(new File("resources/test1.jpg"));
             System.out.println("Is Extension is correct for test1.jpg: " + fileTypeCheck.isExtensionCorrect());
             System.out.println("\n");
 
 
-            FileTypeCheckImpl fileTypeCheck2 = new FileTypeCheckImpl(new File("resources/test2.gif"));
+            FileTypeCheck fileTypeCheck2 = new FileTypeCheckImpl(new File("resources/test2.gif"));
             System.out.println("Is Extension is correct for test2.gif: " + fileTypeCheck2.isExtensionCorrect());
             System.out.println("\n");
 
-            FileTypeCheckImpl fileTypeCheck3 = new FileTypeCheckImpl(new File("resources/test3.png"));
+            FileTypeCheck fileTypeCheck3 = new FileTypeCheckImpl(new File("resources/test3.png"));
             System.out.println("Is Extension is correct for test3.png: " + fileTypeCheck3.isExtensionCorrect());
             System.out.println("\n");
 
-            FileTypeCheckImpl fileTypeCheck4 = new FileTypeCheckImpl(new File("resources/test4.docx"));
+            FileTypeCheck fileTypeCheck4 = new FileTypeCheckImpl(new File("resources/test4.docx"));
             System.out.println("Is Extension is correct for test4.docx: " + fileTypeCheck4.isExtensionCorrect());
             System.out.println("\n");
 
-            FileTypeCheckImpl fileTypeCheck5 = new FileTypeCheckImpl(new File("resources/test5.rtf"));
+            FileTypeCheck fileTypeCheck5 = new FileTypeCheckImpl(new File("resources/test5.rtf"));
             System.out.println("Is Extension is correct for test5.rtf: " + fileTypeCheck5.isExtensionCorrect());
             System.out.println("\n");
 
